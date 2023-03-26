079f8a10125a9e1251db8920611bf9f834f582f2
edwintancm98 <edwintancm98@gmail.com>
2023-03-26 21:37:23 +0800

    mgn and bar correction

--- src/ee4308_hector/src/motion.cpp
+++ src/ee4308_hector/src/motion.cpp
@@ -54,6 +54,87 @@ void cbImu(const sensor_msgs::Imu::ConstPtr &msg)
     uz = msg->linear_acceleration.z;
     
     //// IMPLEMENT IMU ////
+    cv::Matx21d imu_acc = {ux, uy};
+    // X
+    cv::Matx22d F_xk = {
+        1, imu_dt,
+        0, 1
+    };
+    cv::Matx22d W_xk = {
+        -0.5*pow(imu_dt, 2)*cos(A(0)), 0.5*pow(imu_dt, 2)*sin(A(0)),
+        -imu_dt*cos(A(0))            , imu_dt*sin(A(0)) 
+    };
+    cv::Matx22d Qx = {
+        qx, 0 ,
+        0 , qy
+    };
+    X = F_xk*X + W_xk*imu_acc;
+    P_x = F_xk*P_x*F_xk.t() + W_xk*Qx*W_xk.t();
+
+    // Y
+    cv::Matx22d F_yk = {
+        1, imu_dt,
+        0, 1
+    };
+    cv::Matx22d W_yk = {
+        -0.5*pow(imu_dt, 2)*sin(A(0)),-0.5*pow(imu_dt, 2)*cos(A(0)),
+        -imu_dt*sin(A(0))            ,-imu_dt*cos(A(0)) 
+    };
+    cv::Matx22d Qy = {
+        qx, 0 ,
+        0 , qy
+    };
+    Y = F_yk*Y + W_yk*imu_acc;
+    P_y = F_yk*P_y*F_yk.t() + W_yk*Qy*W_yk.t();
+
+    // Z
+    cv::Matx22d F_zk = {
+        1, imu_dt,
+        0, 1
+    };
+    cv::Matx21d W_zk = {0.5*pow(imu_dt, 2), imu_dt};
+    double Qz = qz;
+    Z = F_zk*Z + W_zk*uz;
+    P_z = F_zk*P_z*F_zk.t() + W_zk*Qz*W_zk.t();
+
+    // 
+    cv::Matx22d F_ak = {
+        1, 0,
+        0, 0
+    };
+    cv::Matx21d W_ak = {imu_dt, 1};
+    double Qa = qa;
+    A = F_ak*A + W_ak*uz;
+    P_a = F_ak*P_a*F_ak.t() + W_ak*Qa*W_ak.t();
+}
+
+// --------- EKF CORRECTION ----------
+void ekfCorrectionX(double Y_k, double h, cv::Matx12d H_k, double V_k, double R_k) {
+    cv::Matx21d K_k = P_x*H_k.t() * 
+        (1 / ((H_k*P_x*H_k.t())(0) + V_k*R_k*V_k));
+    X = X + K_k*(Y_k - h);
+    P_x = P_x - K_k*H_k*P_x;
+}
+
+void ekfCorrectionY(double Y_k, double h, cv::Matx12d H_k, double V_k, double R_k) {
+    cv::Matx21d K_k = P_y*H_k.t() * 
+        (1 / ((H_k*P_y*H_k.t())(0) + V_k*R_k*V_k));
+    Y = Y + K_k*(Y_k - h);
+    P_y = P_y - K_k*H_k*P_y;
+}
+
+void ekfCorrectionZ(double Y_k, double h, cv::Matx12d H_k, double V_k, double R_k) {
+    cv::Matx21d K_k = P_z*H_k.t() * 
+        (1 / ((H_k*P_z*H_k.t())(0) + V_k*R_k*V_k));
+    Z = Z + K_k*(Y_k - h);
+    P_z = P_z - K_k*H_k*P_z;
+}
+
+void ekfCorrectionYaw(double Y_k, double h, cv::Matx12d H_k, double V_k, double R_k) {
+    cv::Matx21d K_k = P_z*H_k.t() * 
+        (1 / ((H_k*P_z*H_k.t())(0) + V_k*R_k*V_k));
+    Z = Z + K_k*(Y_k - h);
+    P_z = P_z - K_k*H_k*P_z;
 }
 
 // --------- GPS ----------
@@ -64,67 +145,121 @@ const double DEG2RAD = M_PI / 180;
 const double RAD_POLAR = 6356752.3;
 const double RAD_EQUATOR = 6378137;
 double r_gps_x, r_gps_y, r_gps_z;
+
+cv::Matx31d initial_ECEF = {NaN, NaN, NaN};
+cv::Matx33d rot_m_n = {
+    1,  0,  0,
+    0, -1,  0,
+    0,  0, -1
+};
+// EKF Correction Stuff for GPS
+cv::Matx12d H_gps = {1, 0};
+double V_gps = 1;
+
 void cbGps(const sensor_msgs::NavSatFix::ConstPtr &msg)
 {
     if (!ready)
         return;
 
-    /*
-    //// IMPLEMENT GPS /////
-    double lat = msg->latitude;
-    double lon = msg->longitude;
-    double alt = msg->altitude;
     
+    //// IMPLEMENT GPS /////
+    double lat = msg->latitude; //IN DEGREES
+    double lon = msg->longitude; //IN DEGREES
+    double alt = msg->altitude;
+
+    double lat_rad = DEG2RAD * lat;
+    double lon_rad = DEG2RAD * lon;
+
+    double eccentricity_sq = 1 - pow(RAD_POLAR, 2) / pow(RAD_EQUATOR, 2);
+    double n_phi = RAD_EQUATOR / sqrt(1 - eccentricity_sq * pow(sin(lat_rad), 2));
+    
+    cv::Matx31d ECEF = {
+        (n_phi + alt) * cos(lat_rad) * cos(lon_rad),
+        (n_phi + alt) * cos(lat_rad) * sin(lon_rad),
+        ((pow(RAD_POLAR,2) / pow(RAD_EQUATOR, 2)) * n_phi + alt) * sin(lat_rad)
+    };
+
     // for initial message -- you may need this:
     if (std::isnan(initial_ECEF(0)))
     {   // calculates initial ECEF and returns
         initial_ECEF = ECEF;
         return;
     }
-    */
+    
+    cv::Matx33d rot_e_n = {
+        -sin(lat_rad) * cos(lon_rad), -sin(lon_rad), -cos(lat_rad) * cos(lon_rad),
+        -sin(lat_rad) * sin(lon_rad), cos(lon_rad) , -cos(lat_rad) * sin(lon_rad),
+         cos(lat_rad)               , 0            , -sin(lat_rad)
+    };
+
+    cv::Matx31d local_NED = rot_e_n.t() * (ECEF - initial_ECEF);
+
+    GPS = rot_m_n * local_NED + initial_pos;
+
+    double Y_gps_x = GPS(0,0);
+    double Y_gps_y = GPS(1,0);
+    double Y_gps_z = GPS(2,0);
+
+    double h_gps_x = X(0,0);
+    double h_gps_y = Y(0,0);
+    double h_gps_z = Z(0,0);
+
+    ekfCorrectionX(Y_gps_x, h_gps_x, H_gps, V_gps, r_gps_x);
+    ekfCorrectionY(Y_gps_y, h_gps_y, H_gps, V_gps, r_gps_y);
+    ekfCorrectionZ(Y_gps_z, h_gps_z, H_gps, V_gps, r_gps_z);
 }
 
 // --------- Magnetic ----------
 double a_mgn = NaN;
 double r_mgn_a;
+cv::Matx12d H_mgn = {1, 0};
+double V_mgn = 1;
 void cbMagnet(const geometry_msgs::Vector3Stamped::ConstPtr &msg)
 {
     if (!ready)
         return;
-    
-    /*
+   
     //// IMPLEMENT GPS ////
     double mx = msg->vector.x;
     double my = msg->vector.y;
-    */
+    a_mgn = atan2(my, mx);
+    double h_mgn_a = A(0,0);
+    ekfCorrectionYaw(a_mgn, h_mgn_a, H_mgn, V_mgn, r_mgn_a);
 }
 
 // --------- Baro ----------
 double z_bar = NaN;
 double r_bar_z;
+double V_bar = 1;
+cv::Matx12d H_bar = {1, 0};
 void cbBaro(const hector_uav_msgs::Altimeter::ConstPtr &msg)
 {
     if (!ready)
         return;
-
-    /*
     //// IMPLEMENT BARO ////
      z_bar = msg->altitude;
-    */
+     double h_bar_z = Z(0,0);
+     ekfCorrectionZ(z_bar, h_bar_z, H_bar, V_bar, r_bar_z);
+
 }
 
 // --------- Sonar ----------
 double z_snr = NaN;
 double r_snr_z;
+// EKF Correction Stuff for GPS
+cv::Matx12d H_snr = {1, 0};
+double V_snr = 1;
 void cbSonar(const sensor_msgs::Range::ConstPtr &msg)
 {
     if (!ready)
         return;
 
-    /*
+    
     //// IMPLEMENT SONAR ////
     z_snr = msg->range;
-    */
+    
+    double h_snr = Z(0,0);
+    ekfCorrectionZ(z_snr, h_snr, H_snr, V_snr, r_snr_z);
 }
 
 // --------- GROUND TRUTH ----------
@@ -289,3 +424,4 @@ int main(int argc, char **argv)
     ROS_INFO("HMOTION: ===== END =====");
     return 0;
 }
+
